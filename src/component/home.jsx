c96b9b4ee1e9fb783b08db2bf7b14279e39ef46e
Dede182 <htetshine.htetmkk@gmail.com>
2022-04-17 13:36:31 +0630

    last update

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Product from './products.jsx'
 import { motion } from 'framer-motion'
+import { Carousel } from 'react-bootstrap'
 export const pagetransition = {
     before:{
         opacity:0,
@@ -30,15 +31,31 @@ const Home = () =>{
             initial = "before"
             animate = "after"
             exit = "exit"
-            className="card bg-dark text-white border-0">
-            <img src="assests/d.png" className="card-img ss image-fluid" height = "600px" alt="background image"/>
-            <div className="card-img-overlay d-flex flex-column justify-content-center">
-                <div className="container">
-                 <h5 className="card-title display-3 fw-bolder mb-0">New Season Arrivals</h5>
-                 <p className="card-text lead fs-2">Check out all the trends</p>
-                
-            </div>
-            </div>
+            className="card p-4 border-0">
+            <Carousel fade>
+  <Carousel.Item>
+    <img
+      className="d-block mg w-100"
+      src="assests/pin_10.jpg"
+      alt="First slide"
+    />
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block mg w-100"
+      src="assests/promo.jpg"
+      alt="Second slide"
+    />
+
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block mg w-100"
+      src="assests/Delivery.jpg"
+      alt="Third slide"
+    />
+  </Carousel.Item>
+</Carousel>
            
             </motion.div>
             <Product/>
